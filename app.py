f9124ebf4784914d4850ce5248e247849525c29c
LennyCollie <markus.lenny@freenet.de>
2025-07-20 20:24:43 +0200

    Update app.py

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ import atexit
 
 # --- App & DB Konfiguration ---
 app = Flask(__name__, template_folder='template')
-app.secret_key = os.urandom(24)"MeinAbsolutGeheimerSchluesselFuerDieSuperApp123!"
+app.secret_key = os.getenv('SECRET_KEY')
 
 database_url = os.getenv('DATABASE_URL')
 if database_url and database_url.startswith("postgres://"):
