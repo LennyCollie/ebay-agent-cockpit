e62f8f1a5905eddab96e4f9e92267eba2060d47e
LennyCollie <markus.lenny@freenet.de>
2025-07-27 22:27:28 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,24 +1,15 @@
 import os
-from datetime import datetime
-from flask import Flask, render_template, request, redirect, url_for, session, flash, jsonify
+from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from werkzeug.security import generate_password_hash, check_password_hash
-import stripe
-from dotenv import load_dotenv
 from flask_migrate import Migrate
+from dotenv import load_dotenv
 
+# .env laden
 load_dotenv()
 
-app = Flask(__name__, template_folder='template')
-app.secret_key = os.getenv('SECRET_KEY')
-
-database_url = os.getenv('DATABASE_URL')
-if database_url and database_url.startswith("postgres://"):
-    database_url = database_url.replace("postgres://", "postgresql://", 1)
-    if "sslmode" not in database_url:
-        database_url += "?sslmode=require"
-
-app.config['SQLALCHEMY_DATABASE_URI'] = database_url or 'sqlite:///db.sqlite3'
+app = Flask(__name__)
+app.config['SECRET_KEY'] = os.getenv('SECRET_KEY', 'defaultsecret')
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL', 'sqlite:///db.sqlite3')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
