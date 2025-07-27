045600ef51c3aff25d580f5ed543c8cd2b471f86
LennyCollie <markus.lenny@freenet.de>
2025-07-27 21:43:47 +0200

    Update manage.py

--- manage.py
+++ manage.py
@@ -1,15 +1,9 @@
-from flask_migrate import MigrateCommand
 from flask.cli import AppGroup
 from app import app, db
 import click
-
-
 
 @app.cli.command("create")
 def create():
     """Creates all tables"""
     db.create_all()
     print("Tables created")
-
-if __name__ == "__main__":
-    app.run()
