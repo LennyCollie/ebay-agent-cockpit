b2e30817b6fb915d25bcd62de65e103be19ab27f
LennyCollie <markus.lenny@freenet.de>
2025-08-10 13:06:15 +0200

    Update app.py

--- app.py
+++ app.py
@@ -217,6 +217,14 @@ def settings():
         flash("Einstellungen gespeichert.")
         return redirect(url_for("settings"))
     return render_template("settings.html")
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template("404.html"), 404
+
+@app.errorhandler(500)
+def server_error(e):
+    return render_template("500.html"), 500
     
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 10000))
